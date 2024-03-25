77dc961e7bbf646761a6f38e21879969febe91c5
Michal Tynior <mtynior@future-processing.com>
2024-03-25 10:07:10 +0100

    Initial Commit

---
+++ SparklePlayground/SparklePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparklePlaygroundApp.swift
+//  SparklePlayground
+//
+//  Created by Michal on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SparklePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
